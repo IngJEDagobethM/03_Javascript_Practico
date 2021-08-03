58862b14d7bcf122e80c57e8a5dc0192b26e3e42
Ing.JEDagobethM <x.game19@gmail.com>
2021-08-03 11:49:40 -0500

    Aplicando descuentos con cupones

--- descuentos.js
+++ descuentos.js
@@ -6,10 +6,20 @@ function precioConDescuento(precio, descuento) {
     return ( precio * porcPrecioConDescuento(descuento) ) / 100;
 } 
 
+const coupons = [
+    {valor: "NEW", descuento: 5},
+    {valor: "GOLD", descuento: 15},
+    {valor: "ROYAL", descuento: 20},
+    {valor: "BLACKF", descuento: 17.5}
+];
+
 function onClickButtonPriceDiscount() {
-    let inputPrice = 0;
-    let inputDiscount = 0;
-    inputPrice = document.getElementById("inputPrice").value;
-    inputDiscount = document.getElementById("inputDiscount").value;
-    document.getElementById("resultP").innerText = "El precio con descuento es: " + precioConDescuento(inputPrice, inputDiscount);
+    let inputPrice = document.getElementById("inputPrice").value;
+    let inputDiscount = document.getElementById("inputDiscount").value;
+
+    let encuentraCoupon = coupons.find(function(coupon){
+        return coupon.valor === inputDiscount;
+    });
+
+    document.getElementById("resultP").innerText = "El precio con descuento es: " + precioConDescuento(inputPrice, encuentraCoupon.descuento);
 }
